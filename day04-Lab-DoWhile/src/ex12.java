8ac7cd21e451c8f6fa0d4db26825d42587a62a7e
JonathanG01 <jonathan.gomez@revature.net>
2021-07-16 10:33:07 -0500

    day 04 labs
    day 04 labs.

---
+++ day04-Lab-DoWhile/src/ex12.java
@@ -0,0 +1,13 @@
+
+public class ex12 {
+
+	public static void main(String[] args) {
+boolean on = false;
+    	
+    	do{
+    		System.out.println("Inside the do-while loop");
+    	} while (on);
+	}
+
+	}
+
---
+++ day04-Lab-Method/src/ex16.java
@@ -0,0 +1,17 @@
+
+public class ex16 {
+
+	public static void main(String[] args) {
+		ex16 md = new ex16();
+
+	    //call your first method here
+	    md.talk();
+	    md.talk();
+	    md.talk();
+	}
+	//create your first method here
+    public void talk(){
+        System.out.println("Inside of the talk method");
+    }
+
+}
